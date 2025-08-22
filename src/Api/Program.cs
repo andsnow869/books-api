ef5c02aa67c7979bb135854979270a31bf80dd94
Maryna <kons0370@icloud.com>
2025-08-22 21:52:57 +0100

    Интеграция базы данных

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,4 +1,15 @@
+using Marten;
+
 var builder = WebApplication.CreateBuilder(args);
+//Чтение строки подключения
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")!; //берем из файла Development.json
+
+//Подключение Marten
+builder.Services.AddMarten(option =>   // Add.Marten сервис для работы с PostgreSQL.
+{
+    option.Connection(connectionString); //указываем для Marten, какую строку подключения использовать для Postgres.
+});
+
 var app = builder.Build(); //сборка
 
 
